1ff5d07be2e7a0aceb03bfbeaf5a8091635e8ae9
SimoneSantoro21 <simosant2112@gmail.com>
2023-10-29 20:30:38 +0100

    defined opened_door and other_door

--- monthy_hall.py
+++ monthy_hall.py
@@ -13,15 +13,21 @@ def switch(chosen_door, other_door, bool):
 def open_a_door():
     return 
 
-doors = ['a', 'b', 'c']
+doors = ['Door 1', 'Door 2', 'Door 3']
 
-choice = doors[np.random.randint(1, 4) - 1]
-winning_door = doors[np.random.randint(1, 4) - 1]
+first_choice = np.random.choice(doors)
+winning_door = np.random.choice(doors)
+opened_door = list(set(doors) - set([first_choice, winning_door]))[0]
+other_door = list(set(doors) - set([first_choice, opened_door]))[0]
 
-print("chosen door =", choice)
+print("chosen door = ", first_choice)
+print("Opened door = ", opened_door)
+print("Other door = ", other_door)
 print("winning door =", winning_door)
 
-switch(choice, winning_door, True)
 
-print("chosen door =", choice)
-print("winning door =", winning_door)
+#
+#switch(first_choice, winning_door, True)
+#
+#print("chosen door =", first_choice)
+#print("winning door =", winning_door)
