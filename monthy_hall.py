3bce97c9d3f9824f2353b4604da95c6419a15254
SimoneSantoro21 <simosant2112@gmail.com>
2023-10-29 22:58:28 +0100

    Updated switching conditions

--- monthy_hall.py
+++ monthy_hall.py
@@ -2,32 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def switch(chosen_door, other_door, bool):
-    if(bool == True):
-        a = chosen_door
-        b = other_door
-        chosen_door=b
-        other_door=a
-    #return True
+N = 10000000
+win_no_switch = 0
+win_switch = 0
 
-def open_a_door():
-    return 
+for i in range(N):
+    doors = ['Door 1', 'Door 2', 'Door 3']
 
-doors = ['Door 1', 'Door 2', 'Door 3']
+    first_choice = np.random.choice(doors)
+    winning_door = np.random.choice(doors)
+    opened_door = list(set(doors) - set([first_choice, winning_door]))[0]
+    other_door = list(set(doors) - set([first_choice, opened_door]))[0]
 
-first_choice = np.random.choice(doors)
-winning_door = np.random.choice(doors)
-opened_door = list(set(doors) - set([first_choice, winning_door]))[0]
-other_door = list(set(doors) - set([first_choice, opened_door]))[0]
+    if(first_choice == winning_door):
+        win_no_switch += 1
 
-print("chosen door = ", first_choice)
-print("Opened door = ", opened_door)
-print("Other door = ", other_door)
-print("winning door =", winning_door)
+    first_choice = other_door
+    if(first_choice == winning_door):
+        win_switch += 1
 
 
-#
-#switch(first_choice, winning_door, True)
-#
-#print("chosen door =", first_choice)
-#print("winning door =", winning_door)
+print("By switching, the measured probability of success is p_switch = ", win_switch / N)
+print("By keeping the first choice, the measured probability of success is p_no_switch = ", win_no_switch / N)
