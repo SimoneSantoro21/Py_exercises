1c4840a80a84e83163bf89af89c87e52d2e36e24
SimoneSantoro21 <simosant2112@gmail.com>
2023-11-09 10:53:36 +0100

    implemented N_particles

--- markov_chain.py
+++ markov_chain.py
@@ -1,10 +1,14 @@
 import numpy as np
 
-N = 100000
-N_particles = 1
-states = ["State 1", "State 2", "State 3"]
-particles = np.zeros(N_particles, dtype=str)
-particles = states[0]
+#defining variables 
+N = 1000000                                  #number of iterations per particle
+N_particles = 10000                             #number of particles
+states = ["State 1", "State 2", "State 3"]  #Possible states
+n_state_1 = 0                               #
+n_state_2 = 0                               #outcome counters
+n_state_3 = 0                               #
+trajectory = []                    #trajectory of the outcomes, not differentiated by the particle
+particles = [[]*N]*N_particles 
 
 #transition matrix 
 pi = [[1/2, 1/3, 1/2],
@@ -37,26 +41,23 @@ def transition(init_condition, transition_matrix):
             new_state = new_state
     return new_state
 
+#for cyccle for different particles, define a particles list etc
+for particle in range(N_particles):
+    particles[particle][0] = np.random.choice(states)
+    trajectory[particle] = particles[particle][0]
+    for step in range(N):
+        trajectory.append(transition(particles[particle][step], pi))
+        if (trajectory[step] == states[0]):
+            n_state_1 += 1
+        if (trajectory[step] == states[1]):
+            n_state_2 += 1
+        if (trajectory[step] == states[2]):
+            n_state_3 += 1
 
-trajectory = [states[0]]
-
-for step in range(N):
-    trajectory.append(transition(particles, pi))
-
-n_state_1 = 0
-n_state_2 = 0
-n_state_3 = 0
-
-for i in range(N):
-    if (trajectory[i] == states[0]):
-        n_state_1 += 1
-    if (trajectory[i] == states[1]):
-        n_state_2 += 1
-    if (trajectory[i] == states[2]):
-        n_state_3 += 1
 
 ro_eq = [n_state_1 / N, n_state_2 / N, n_state_3 / N]
 
 print("At equilibrium, probability vector ro = ", ro_eq,
-       "With number iteration N = ", N)
+       "\n With number iteration N = ", N,
+       "\n and number of particles N_particles = ", N_particles)
     
