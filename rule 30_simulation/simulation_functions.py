5ef926babf8d2da53a71cd40a99b813a167b6178
SimoneSantoro21 <simosant2112@gmail.com>
2024-03-13 19:31:58 +0100

    Implemented functions evolve and find_neighbors Implemented evolve function for computing the oyutput based on the previous state and the rule selected. At each iteration, the neighbors are found using  find_neighbors function. IMPORTANT: in this implementation, the borders of the state are considered as circular for example: the neighbors of "a" in the string "abcde" would be "eab"

--- rule 30_simulation/simulation_functions.py
+++ rule 30_simulation/simulation_functions.py
@@ -13,23 +13,36 @@ def select_rule(num):
     184: {"...": ".", "..0": "0", ".0.": ".", ".00": ".",
            "0..": ".", "0.0": "0", "00.": "0", "000": "0"}
      }
-    
-    return RULES.get(num)
+    return RULES[num]
     
 
-def evolve(oldstate):
+def find_neighbors(index, oldstate):
+    "Find the neighborhood of a cell based on it's position (index)"
+    if index == 0:
+        neighbors = oldstate[-1] + oldstate[0] + oldstate[1]
+    elif index == len(oldstate) - 1:
+        neighbors = oldstate[index - 1] + oldstate[index] + oldstate[0]
+    else:
+        neighbors = oldstate[index - 1] + oldstate[index] + oldstate[index + 1]
+    return neighbors
+
+
+def evolve(oldstate, n_rule):
     "Evaluate  the evolution of a state, based on the selected rule"
+    rule = select_rule(n_rule)
     newstate = ""
+    for i in range(len(oldstate)):
+        neighbors = find_neighbors(i, oldstate)
+        newstate += rule[neighbors]
     return newstate
 
 
 def simulation(nsteps, n_rule):
     "This function performs the actual simulation returning the history of the system"
     initial_state = generate_state()
-    rule = select_rule(n_rule)
     state_history = [initial_state]
     for i in range(nsteps):
         old_state = state_history[-1]
-        new_state = evolve(old_state)
+        new_state = evolve(old_state, n_rule)
         state_history.append(new_state)
     return state_history
--- rule 30_simulation/test_simulation.py
+++ rule 30_simulation/test_simulation.py
@@ -11,8 +11,23 @@ def test_generated_only_one_alive():
     num_alive = sum(1 for i in state if i == '0')
     assert num_alive == 1
 
+def test_correct_neighborhood_circular_borders():
+    "Testing that the neighborhood used to compute the new state for every cell is correct, treating the states as circular"
+    state = "abcdefg"
+    assert func.find_neighbors(0, state) == "gab"
+    assert func.find_neighbors(len(state) - 1, state) == "fga"
+    assert func.find_neighbors(2, state) == "bcd"
+    assert func.find_neighbors(3, state) == "cde"
+
+
 def test_newstate_dimension_equal_to_oldstate():
     "Testing that the evolved state has the same dimension as the old state"
     old_state = func.generate_state()
-    new_state = func.evolve(old_state)
-    assert old_state.len() == new_state.len()
+    new_state = func.evolve(old_state, 30)
+    assert len(old_state) == len(new_state)
+
+
+def test_correct_newstate():
+    "Testing that the newstate is the expected one"
+    state = ".....0....."
+    assert func.evolve(state, 30) == "000000.0000"
